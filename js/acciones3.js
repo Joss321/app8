00c9d5a3dbd57acdf35ad6ad17bc587994c6b6dd
araceliflores01 <38224775+araceliflores01@users.noreply.github.com>
2018-05-28 07:29:03 -0500

    2

---
+++ js/acciones3.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",function(){ //escuchador de eventos
+		$('#izquierda').on("swipeleft",function(){ //deslizar ala izquierda
+			navigator.notificacion.alert("Deslizó hacia la izquierda",function(){"aplicación 7","ok"}); 
+		});//cierra swipe izquierda
+		$('#derecha').on("swiperight",function(){ // dezlizar derecha
+			navigator.notification.confirm("¿Que quieres hacer?",function(op){
+				switch(op) //valiable op para las oociones
+				{
+					case 1:
+					navigator.notificacion.beep(1); // suena el dispositivo
+					break;
+					
+					case 2:
+					navigator.notification.vibrate(3000); //vibra el dispositivo
+					break;
+				}
+			},"aplicacion7","Beepear,Vibrar,Cancelar");
+			});
+	},false);									
+});
