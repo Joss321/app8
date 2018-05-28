e7dc34297743c05e69e7ddcd964c7f64e2c974db
araceliflores01 <38224775+araceliflores01@users.noreply.github.com>
2018-05-28 07:40:10 -0500

    3

--- js/acciones3.js
+++ js/acciones3.js
@@ -2,14 +2,14 @@
 $(document).ready(function(e) {
     document.addEventListener("deviceready",function(){ //escuchador de eventos
 		$('#izquierda').on("swipeleft",function(){ //deslizar ala izquierda
-			navigator.notificacion.alert("Deslizó hacia la izquierda",function(){"aplicación 7","ok"}); 
+			navigator.notification.alert("Deslizó hacia la izquierda",function(){"aplicación 7","ok"}); 
 		});//cierra swipe izquierda
 		$('#derecha').on("swiperight",function(){ // dezlizar derecha
 			navigator.notification.confirm("¿Que quieres hacer?",function(op){
 				switch(op) //valiable op para las oociones
 				{
 					case 1:
-					navigator.notificacion.beep(1); // suena el dispositivo
+					navigator.notification.beep(1); // suena el dispositivo
 					break;
 					
 					case 2:
